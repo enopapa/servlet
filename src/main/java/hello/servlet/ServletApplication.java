8d08f15b7fdbc29749d67dd3a24516d624cf9274
enopapa <enopapa@naver.com>
2022-06-20 00:00:53 +0900

    web application servlet
    jsp mvc pattern test #6

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -1,8 +1,10 @@
 package hello.servlet;
 
+import hello.servlet.web.springmvc.v1.SpringMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @ServletComponentScan	// 서블릿 자동 등록
 @SpringBootApplication
@@ -12,4 +14,10 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
+	// spring bean 직접 등록
+//
+//	@Bean
+//	SpringMemberFormControllerV1 springMemberFormControllerV1()	{
+//		return new SpringMemberFormControllerV1();
+//	}
 }
